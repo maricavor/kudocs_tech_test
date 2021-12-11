4b7718ed39b41203ba5d3d1f8cbe0dd3444b71f9
maricavor <sergeit6@gmail.com>
2021-12-11 11:31:01 +0200

    Initial commit

--- test.rb
+++ test.rb
@@ -9,5 +9,5 @@ calculator = PageviewCalculator.new('production.log')
 p 'A list of web pages with the most page views:'
 calculator.list_most_viewed.each { |p| puts p }
 
-p 'A list of most unique page views:'
+p 'A list of web pages with most unique views:'
 calculator.list_most_unique.each { |v| puts v }
